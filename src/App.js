feba8128f493d41545be17666315877697d6765a
HariPrasath-369 <hariprasathmano2004@gmail.com>
2026-03-03 20:27:01 +0530

    second

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import QuizPage from './component/Quiz-page';
 import FetchQuestion from './component/fetch-Questions';
 import QuestionPage from './component/Question-page';
 import ResultPage from './component/ResultPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<QuizPage />} />
         <Route path="/loading" element={<FetchQuestion />} />
         <Route path="/quiz" element={<QuestionPage />} />
         <Route path="/result" element={<ResultPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
